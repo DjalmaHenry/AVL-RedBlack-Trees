d97e204891f44352dbca04563e41cdea89767b13
Djalma Henrique Silva Lima <djalma@manifestohut.com.br>
2021-10-29 12:17:12 -0300

    feat: add balance AVL method

--- trees-projects/projeto-arvores/src/main/java/br/unicap/ed2/pv/ArvorePV.java
+++ trees-projects/projeto-arvores/src/main/java/br/unicap/ed2/pv/ArvorePV.java
@@ -12,9 +12,37 @@ public class ArvorePV extends AbstractArvoreBinariaDePesquisa<NoPV> {
 
     @Override
     public NoPV inserir(int key) {
-        NoPV n = inserirNoHelper(raiz, key);
-        //Altera
-        return n;
+        NoPV h = inserirNoHelper(raiz, key);
+        if(h == null) return new RedBlackNode<>(data);
+        int c = comp.compare(data, h.getData());
+        if(c < 0)
+        {
+            h.setEsquerda(inserir(data, h.getEsquerda()));
+        }
+        else if(c > 0)
+        {
+            h.setDireita(inserir(data, h.getDireita()));
+        }
+        else
+        {
+            h.setData(data);
+        }
+        //Now for the rotating
+        if(isRed(h.getRight()) && !isRed(h.getLeft()))
+        {
+            h = rotateLeft(h);
+        }
+        
+        if(isRed(h.getLeft()) && isRed(h.getLeft().getLeft()))
+        {
+            h = rotateRight(h);
+        }
+        
+        if(isRed(h.getLeft()) && isRed(h.getRight()))
+        {
+            flipColors(h);
+        }
+        return h;
     }
 
     @Override
