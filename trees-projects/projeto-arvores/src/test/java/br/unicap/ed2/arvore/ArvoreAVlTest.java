c377be3aab4205dc1e7fd18b35197fdb32adf16c
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2021-10-31 16:46:25 -0300

    Comentários RR e RL meio

--- trees-projects/projeto-arvores/src/test/java/br/unicap/ed2/arvore/ArvoreAVlTest.java
+++ trees-projects/projeto-arvores/src/test/java/br/unicap/ed2/arvore/ArvoreAVlTest.java
@@ -28,36 +28,35 @@ public class ArvoreAVlTest {
     }
 
     @Test
-    public void testaLLMeio()
-    {
+    public void testaLLMeio() {
         AVL a = new AVL();
-        a.inserir(10,a.getRaiz());
-        a.inserir(20,a.getRaiz());
-        a.inserir(5,a.getRaiz());
-        a.inserir(4,a.getRaiz());
-        a.inserir(7,a.getRaiz());
-        a.inserir(30,a.getRaiz());
-        a.inserir(15,a.getRaiz());
-        a.inserir(2,a.getRaiz());
-        a.inserir(1,a.getRaiz());
-        NoAVL aux = a.procurar(2,a.getRaiz());
-        assertTrue( aux != null );
+        a.inserir(10, a.getRaiz());
+        a.inserir(20, a.getRaiz());
+        a.inserir(5, a.getRaiz());
+        a.inserir(4, a.getRaiz());
+        a.inserir(7, a.getRaiz());
+        a.inserir(30, a.getRaiz());
+        a.inserir(15, a.getRaiz());
+        a.inserir(2, a.getRaiz());
+        a.inserir(1, a.getRaiz());
+        NoAVL aux = a.procurar(2, a.getRaiz());
+        assertTrue(aux != null);
         NoAVL auxDir = aux.getDir();
         NoAVL auxEsq = aux.getEsq();
-        assertTrue( auxEsq != null );
-        assertTrue( auxEsq.getInfo() == 1 );
-        assertTrue( auxDir != null );
-        assertTrue( auxDir.getInfo() == 4 );
+        assertTrue(auxEsq != null);
+        assertTrue(auxEsq.getInfo() == 1);
+        assertTrue(auxDir != null);
+        assertTrue(auxDir.getInfo() == 4);
 
     }
 
     @Test
     public void testaRR2Raiz() {
         AVL a = new AVL();
-        a.inserir(1,a.getRaiz());
-        a.inserir(2,a.getRaiz());
-        a.inserir(3,a.getRaiz());
-        NoAVL aux = a.procurar(2,a.getRaiz());
+        a.inserir(1, a.getRaiz());
+        a.inserir(2, a.getRaiz());
+        a.inserir(3, a.getRaiz());
+        NoAVL aux = a.procurar(2, a.getRaiz());
         assertTrue(aux != null);
         NoAVL auxDir = aux.getDir();
         NoAVL auxEsq = aux.getEsq();
@@ -72,16 +71,19 @@ public class ArvoreAVlTest {
     @Test
     public void testaRRMeio() {
         AVL a = new AVL();
-        a.inserir(50,a.getRaiz());
-        a.inserir(10,a.getRaiz());
-        a.inserir(60,a.getRaiz());
-        a.inserir(20,a.getRaiz());
-        a.inserir(5,a.getRaiz());
-        a.inserir(70,a.getRaiz());
-        a.inserir(40,a.getRaiz());
-        a.inserir(80,a.getRaiz());
-        a.inserir(90,a.getRaiz());
-        NoAVL aux = a.procurar(80,a.getRaiz());
+        a.inserir(50, a.getRaiz());
+        a.inserir(10, a.getRaiz());
+        a.inserir(60, a.getRaiz());
+        a.inserir(20, a.getRaiz());
+        a.inserir(5, a.getRaiz());
+        a.inserir(70, a.getRaiz());
+        a.inserir(40, a.getRaiz());
+        // Professor após a insercção do 80 o nó 60 fica desbalanceado (2),
+        // então se faz necessário uma rotação a esquerda
+        a.inserir(80, a.getRaiz());
+        a.inserir(90, a.getRaiz());
+        // Enfim, o 80 fica só com a subárvore a direita que é o 90
+        NoAVL aux = a.procurar(80, a.getRaiz());
         assertTrue(aux != null);
         NoAVL auxDir = aux.getDir();
         NoAVL auxEsq = aux.getEsq();
@@ -95,17 +97,17 @@ public class ArvoreAVlTest {
     @Test
     public void testaLRMeio() {
         AVL a = new AVL();
-        a.inserir(10,a.getRaiz());
-        a.inserir(20,a.getRaiz());
-        a.inserir(5,a.getRaiz());
-        a.inserir(4,a.getRaiz());
-        a.inserir(7,a.getRaiz());
-        a.inserir(30,a.getRaiz());
-        a.inserir(15,a.getRaiz());
-        a.inserir(2,a.getRaiz());
-        a.inserir(3,a.getRaiz());
+        a.inserir(10, a.getRaiz());
+        a.inserir(20, a.getRaiz());
+        a.inserir(5, a.getRaiz());
+        a.inserir(4, a.getRaiz());
+        a.inserir(7, a.getRaiz());
+        a.inserir(30, a.getRaiz());
+        a.inserir(15, a.getRaiz());
+        a.inserir(2, a.getRaiz());
+        a.inserir(3, a.getRaiz());
 
-        NoAVL aux = a.procurar(3,a.getRaiz());
+        NoAVL aux = a.procurar(3, a.getRaiz());
         assertTrue(aux != null);
         NoAVL auxDir = aux.getDir();
         NoAVL auxEsq = aux.getEsq();
@@ -119,16 +121,19 @@ public class ArvoreAVlTest {
     @Test
     public void testaRLMeio() {
         AVL a = new AVL();
-        a.inserir(50,a.getRaiz());
-        a.inserir(10,a.getRaiz());
-        a.inserir(60,a.getRaiz());
-        a.inserir(20,a.getRaiz());
-        a.inserir(5,a.getRaiz());
-        a.inserir(70,a.getRaiz());
-        a.inserir(40,a.getRaiz());
-        a.inserir(80,a.getRaiz());
-        a.inserir(75,a.getRaiz());
-        NoAVL aux = a.procurar(75,a.getRaiz());
+        a.inserir(50, a.getRaiz());
+        a.inserir(10, a.getRaiz());
+        a.inserir(60, a.getRaiz());
+        a.inserir(20, a.getRaiz());
+        a.inserir(5, a.getRaiz());
+        a.inserir(70, a.getRaiz());
+        a.inserir(40, a.getRaiz());
+        a.inserir(80, a.getRaiz());
+        // Professor após a insercção do 80 o nó 60 fica desbalanceado (2),
+        // então se faz necessário uma rotação a esquerda
+        a.inserir(75, a.getRaiz());
+        // transformando o nó 75 em folha(sem filhos)
+        NoAVL aux = a.procurar(75, a.getRaiz());
         assertTrue(aux != null);
         NoAVL auxDir = aux.getDir();
         NoAVL auxEsq = aux.getEsq();
